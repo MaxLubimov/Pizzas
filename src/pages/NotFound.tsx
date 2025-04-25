f3562287e41c106f3a12d44ff1d072dbbfa5f7cf
MaxLubimov <vi-rus-@mail.ru>
2025-04-25 18:07:46 +0300

    Updates

--- src/pages/NotFound.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import NotFoundBlock from '../components/NotFound';
-
-const NotFound: React.FC = () => <NotFoundBlock />;
-
-export default NotFound;
--- src/utils/getCartFromLS.ts
+++
@@ -1,4 +0,0 @@
-export const getCartFromLS = () => {
-  const items = localStorage.getItem('cart');
-  return items ? JSON.parse(items) : [];
-};
